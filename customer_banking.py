469ae76679ad3caecea9bfec46e88e1fe993f51c
saracmoujahed <scmoujahed@gmail.com>
2024-11-04 09:02:25 -0500

    assignment complete

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from savings_account import create_savings_account
+from cd_account import create_cd_account
 
 # Define the main function
 def main():
@@ -9,21 +11,28 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
-
+    savings_balance = input("What is the balance of your savings account? ")
+    savings_interest = input("What is the interest rate on this account? ")
+    savings_maturity = input("How many months has the account been accruing interest? ")
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-
+    print(f"Your interest earned is ${interest_earned:0.2f} \nWe have updated your savings account balance to ${updated_savings_balance:0,.2f}")
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
-
+    cd_balance = input("What is the balance of your CD account? ")
+    cd_interest = input("What is the interest rate on this account? ")
+    cd_maturity = input("How many months has the account been accruing interest? ")
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"Your interest earned is ${interest_earned:0.2f} \nWe have updated your CD account balance to ${updated_cd_balance:0,.2f}")
+
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
